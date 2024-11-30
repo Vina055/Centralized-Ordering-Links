763982ff5c2b0b6deea1d4f6f93dedc0fb45febb
JayChang <weru6129@gmail.com>
2024-11-30 18:57:08 +0800

    modified:   restaurant-detail.css 	modified:   restaurant-detail.html 	modified:   restaurant-detail.js add same category lists

--- restaurant-detail.js
+++ restaurant-detail.js
@@ -11,10 +11,10 @@ if (restaurant) {
   // 渲染餐廳的詳細資料
   const restaurantInfo = document.getElementById('detail-container');
   const card = document.createElement("div");
-  card.className = "detail-card";
+  card.className = "product-container";
 
   card.innerHTML = `
-    <div class="product-container">
+    <div class="product-info">
       <div class="product-image">
           <img src="${restaurant.image}" alt="商品圖片">
       </div>
@@ -34,6 +34,63 @@ if (restaurant) {
     </div>
   `;
   restaurantInfo.appendChild(card);
+
+  // 根據找到的餐廳名稱找到對應的同類餐廳
+  const sameCategory = restaurants.filter(r => r.type === restaurant.type);
+  console.log(sameCategory);
+
+  
+  if(sameCategory.length != 1)
+  {
+    
+    const textInfo = document.getElementById('text-container');
+
+    // 取得目前已儲存的最愛餐廳
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  
+    // 渲染同類餐廳的小卡
+    const moreRestaurant = document.getElementById('same-category-container');
+    moreRestaurant.innerHTML = "";
+    
+    // 新增 "更多有關:" 的文字
+    const text = document.createElement('div');
+    text.innerHTML = `<h1 class="product-title">更多有關${restaurant.type}</h1>`;
+    textInfo.appendChild(text);
+  
+    sameCategory.forEach(shop => {
+      if(shop.name === restaurantName) {
+        return;
+      } 
+      const item = document.createElement("div");
+      item.className = "relevant-item";
+  
+      // 檢查餐廳是否已經在最愛
+      const isFavorite = favorites.some(fav => fav.name === restaurant.name);
+      
+      item.innerHTML = `
+        <div class="restaurant-card">
+          <!-- 圖片和跳轉功能 -->
+          <a href="restaurant-detail.html?name=${encodeURIComponent(shop.name)}" class="restaurant-image" 
+             style="background-image: url('${shop.image}');">
+          </a>
+  
+          <!-- 餐廳資訊 -->
+          <div class="restaurant-info">
+            <h3>${shop.name}</h3>
+            <p>${shop.type}</p>
+          </div>
+  
+          <!-- 按鈕功能 -->
+          <div class="restaurant-actions">
+            <button class="action-button" data-name="${shop.name}">
+              ${isFavorite ? '❤️' : '🤍'}
+            </button>
+          </div>
+        </div>
+      `;
+      moreRestaurant.appendChild(item);
+    });
+  }
 } else {
   // 如果找不到餐廳資料，顯示錯誤訊息
   const restaurantInfo = document.getElementById('restaurant-info');
